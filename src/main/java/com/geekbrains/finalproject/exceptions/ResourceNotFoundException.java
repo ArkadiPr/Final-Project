e71f09bebd346a8490fd179e171f9bf5e985279b
Arkady <arkadikostandov@gmail.com>
2020-09-05 19:01:46 +0300

    final project init

--- src/main/java/com/geekbrains/finalproject/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.finalproject.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
