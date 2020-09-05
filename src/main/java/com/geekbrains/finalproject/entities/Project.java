0ebfb6c2136749852dd76851494e66b995b20d24
Arkady <arkadikostandov@gmail.com>
2020-09-05 20:06:33 +0300

    add entities

---
+++ src/main/java/com/geekbrains/finalproject/entities/Project.java
@@ -0,0 +1,26 @@
+package com.geekbrains.finalproject.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "project")
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    private List<Task> tasks;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
