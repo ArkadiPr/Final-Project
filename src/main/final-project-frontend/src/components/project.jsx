0033f2717540438aadc600ef7a5aa830c1d1d8c3
Daniil <davydovdanya18@gmail.com>
2020-09-10 16:45:11 +0300

    added basic elements of project page

--- src/main/final-project-frontend/src/components/project.jsx
+++ src/main/final-project-frontend/src/components/project.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-
+import axios from 'axios';
+import authHeader from '../api/authHeader';
+import TextField from '@material-ui/core/TextField';
 const Project = (props) =>{
+    const API_URL = 'http://localhost:8188/api/v1/projects/';
     const {setPage, projectId} = props;
-    
+    const [project, setProject] = useState(null);
+
+    useEffect(()=>{
+        return axios.get(API_URL + projectId, 
+            {headers: authHeader()})
+            .then(res => { 
+                console.log(res);
+                setProject(res.data);
+            });
+    },[]);
+
     const switchToCreateProject = (e) => {
         e.preventDefault();
         setPage(3);
@@ -17,8 +30,30 @@ const Project = (props) =>{
     return (
         <div>
             <h3>Project</h3>
-            <Button onClick={switchToCreateProject}>CreateProject</Button>
-            <Button onClick={switchToProjects}>Projects</Button>
+            <div className="navbar">
+                <Button onClick={switchToCreateProject}>CreateProject</Button>
+                <Button onClick={switchToProjects}>Projects</Button>
+            </div>
+            <div>
+                <label>Project name: </label>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined">{project.projectName}</TextField>
+                <Button>Edit</Button>
+            </div>
+            <div>
+                <label>Owner: </label>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined">{project.user.username}</TextField>
+            </div>
+            <div>
+                <label>Tasks: </label> 
+                <ul>
+                    {/* {project.tasks.map(item => (
+                        <li>
+                            <Button onClick={e => openTask(e, item.id)}>{item.title} : {item.priority} : {item.status}</Button>
+                        </li>
+                    ))} */}
+                </ul>
+                <Button>Add new task</Button>
+            </div>
         </div>
     );
 }
