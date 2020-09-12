81bd81183af9b62fb9bd1a21ce6b595e2ddebbe3
Daniil <davydovdanya18@gmail.com>
2020-09-12 21:53:50 +0300

    edite createProject.jsx

--- src/main/final-project-frontend/src/components/createProject.jsx
+++ src/main/final-project-frontend/src/components/createProject.jsx
@@ -4,47 +4,114 @@ import axios from 'axios';
 import authHeader from '../api/authHeader';
 import authController from '../api/authController';
 import TextField from '@material-ui/core/TextField';
+import history from '../history';
+import { Form, Field, List, Submit } from 'easy-react-form';
 
 const CreateProject = (props) => {
     const API_URL = 'http://localhost:8188/api/v1/projects/';
     const user = authController.getCurrentUser();
-    const {setPage, setProjectId} = props;
-    const {projectName, setProjectName} = useState('');
-
-    useEffect(()=>{
-        console.log(user);
-    },[]);
-
+    const [projectName, setProjectName] = useState('');
+    const [tasks, setTasks] = useState([]);
+    const users = [];
+    const [isTasksCreated, setTrue] = useState(false);
     const switchToProjects = (e) => {
         e.preventDefault();
-        setPage(1);
+        history.push("/projects");
+        window.location.reload();
     };
 
-    const openNewProject = (e) => {
+    const createNewProject = (e) => {
         e.preventDefault();
-        const newProject = {id: null, projectName: projectName, user: user, tasks: null};
-        return axios.post(API_URL, newProject, 
-            {headers: authHeader()})
-            .then(res => { 
-                setProjectId(res.id);
-                setPage(2);
-        });
+        if(!isTasksCreated) {
+            console.log("Tasks weren't created!");
+        }
+        else {
+            console.log(tasks);
+            addTask(tasks);
+            const newProject = {projectName: projectName, tasks: tasks};
+            return axios.post(API_URL, newProject, 
+                {headers: authHeader()})
+                .then(res => { 
+                    console.log(res.data);
+            });
+        }
+    };
+
+    const addTask = (tasks) => {
+        setTrue(true);
+        setTasks(tasks);
     };
 
     return (
         <div>
             <h3>Create Project</h3>
+            <div>
             <Button onClick={switchToProjects}>Projects</Button>
-            <form onSubmit={openNewProject}>
+            </div>
                 <label>Project name:</label>
                 <div>
-                    <TextField id="filled-basic" label="Filled" variant="filled" onChange={e=>setProjectName(e.target.value)} />
+                    <TextField id="filled-basic" label="Filled" variant="filled" onChange={e=>setProjectName(e.target.value)} ></TextField>
                 </div>
                 <div>
-                    <Button type="submit">Create</Button>
+                <Form onSubmit={addTask}>
+                    <h3>
+                        The list of tasks:
+                    </h3>
+                    <List name="tasks">
+                        {(tasks) => (
+                        <React.Fragment>
+                            {tasks.map((i) => (
+                            <React.Fragment>
+                                <div>
+                                    <Field
+                                    i={i}
+                                    component="input"
+                                    name="title"/>
+                                </div>
+
+                                <div>
+                                    <Field
+                                    i={i}
+                                    component="input"
+                                    name="description"/>
+                                </div>
+
+                                <div>
+                                    <Field
+                                    i={i}
+                                    component="input"
+                                    name="users"/>
+                                </div>
+
+                                <button onClick={() => tasks.remove(i)}>
+                                Remove
+                                </button>
+                            </React.Fragment>
+                            ))}
+                            <button onClick={tasks.add}>
+                            Add
+                            </button>
+                        </React.Fragment>
+                        )}
+                    </List>
+                    <Submit component={ SubmitButton }>
+                        Create all tasks
+                    </Submit>
+                </Form>
                 </div>
-            </form>
+                <div>
+                    <Button onClick={createNewProject}>Create project</Button>
+                </div>
         </div>
     );
 }
+
+function SubmitButton({ wait, children }) {
+    return (
+      <button disabled={ wait }>
+        { children }
+      </button>
+    )
+}
+
 export default CreateProject;
