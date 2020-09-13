67a58bc147fb497bfd6c538408b2a7440f645589
Arkady <arkadikostandov@gmail.com>
2020-09-13 21:30:14 +0300

    add task dto

--- src/main/java/com/geekbrains/finalproject/controllers/TaskController.java
+++ src/main/java/com/geekbrains/finalproject/controllers/TaskController.java
@@ -1,8 +1,11 @@
 package com.geekbrains.finalproject.controllers;
 
 import com.geekbrains.finalproject.entities.Task;
+import com.geekbrains.finalproject.entities.User;
+import com.geekbrains.finalproject.entities.dtos.TaskDto;
 import com.geekbrains.finalproject.exceptions.ResourceNotFoundException;
 import com.geekbrains.finalproject.services.TaskService;
+import com.geekbrains.finalproject.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,8 @@ import java.util.List;
 @AllArgsConstructor
 public class TaskController {
     private TaskService taskService;
+    private UserService userService;
+
 
     @GetMapping
     public List<Task> getAllTasks() {
@@ -42,6 +47,18 @@ public class TaskController {
         }
         return taskService.saveOrUpdate(task);
     }
+
+    @GetMapping("/{username}")
+    public Task modifyTaskAddExecutor(@PathVariable TaskDto taskDto) {
+        User user=userService.findByUsername(taskDto.getUsername()).orElse(null);
+        Task task=taskService.findById(taskDto.getId());
+        if(user!=null){
+            task.getUsers().add(user);
+        }
+        return task;
+    }
+
+
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id) {
         taskService.deleteById(id);
---
+++ src/main/java/com/geekbrains/finalproject/entities/dtos/TaskDto.java
@@ -0,0 +1,11 @@
+package com.geekbrains.finalproject.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class TaskDto {
+    private Long id;
+    private String username;
+}
