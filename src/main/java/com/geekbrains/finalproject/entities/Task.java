e8fe34062d33c994f43e781d48f836895e915d9a
Daniil <davydovdanya18@gmail.com>
2020-09-14 16:39:16 +0300

    fixed bug deleteById

--- src/main/java/com/geekbrains/finalproject/entities/Task.java
+++ src/main/java/com/geekbrains/finalproject/entities/Task.java
@@ -51,8 +51,7 @@ public class Task {
     @JoinColumn(name = "project_id")
     private Project project;
 
-    @OneToMany(mappedBy = "task")
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @OneToMany(mappedBy = "task", cascade = CascadeType.REMOVE)
     private List<Comment> comments;
 
 
