ea171628784e3916e04668e74a7c346d1c867aee
Daniil <davydovdanya18@gmail.com>
2020-09-14 15:14:19 +0300

    added table comments

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -46,16 +46,25 @@ project_id bigint references projects(id),
 created_at TIMESTAMP DEFAULT current_timestamp
 );
 
-insert into tasks(title, description, status, priority ,project_id)
+insert into tasks (title, description, status, priority ,project_id)
 values ('task1', 'description', 'IS_CREATE' , ' IN_THE_PLANS' , 1),
        ('task2', 'description', 'IS_CREATE' , ' IN_THE_PLANS' , 1),
        ('task3', 'description', 'IS_CREATE' , ' IN_THE_PLANS' , 2),
        ('task4', 'description', 'IS_CREATE' , ' IN_THE_PLANS' , 3);
 
-create table users_tasks(user_id bigint references users(id), task_id bigint references tasks(id));
+create table users_tasks (user_id bigint references users(id), task_id bigint references tasks(id));
 
 insert into users_tasks(user_id, task_id)
 values (1, 2),
        (3, 1),
        (2, 3),
        (2, 4);
+
+create table comments (
+id bigserial primary key,
+user_id bigint references users(id),
+to_user varchar(255),
+task_id bigint references tasks(id),
+text text,
+created_at TIMESTAMP DEFAULT current_timestamp
+);
